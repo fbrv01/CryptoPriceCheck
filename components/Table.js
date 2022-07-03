92c9f95e4d5c17b8419bd6e9caaa17176e125460
fbrv01 <fbucholc@gmail.com>
2022-07-03 23:53:48 +0200

    added fetching from API

--- components/Table.js
+++ components/Table.js
@@ -6,45 +6,58 @@ const { useState, useEffect } = React;
 const { Box, Text, Newline } = require('ink');
 // Destructuring the components we need from ink
 
-const cryptoData = require('../data.json');
-// Fetching mock data
+const axios = require('axios')
+// Used to fetch API data
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum%2Ctether%2Cbnb%2Cxrp%2Ccardano%2Csolana%2Cdogecoin%2Cpolkadot%2Ctron&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
 const Table = () => {
 
-    const [data, setData] = useState([]);
+    const [data, setData] = useState([])
 
     useEffect(()=>{
-        setData(cryptoData);
-    });
+        axios.get(url)
+        .then(response => setData(response.data))
+        .catch(e => console.log(e))
+    },[])
+    // Fetching data and catching possible errors
 
     return (
-        <Box borderStyle='single' padding={2} flexDirection='column'>
-            <Box>
-                <Box width='25%'><Text>COIN</Text></Box>
-                <Box width='25%'><Text>PRICE (USD)</Text></Box>
-                <Box width='25%'><Text>24 HOUR CHANGE</Text></Box>
-                <Box width='25%'><Text>ALL TIME HIGH</Text></Box>
-            </Box>
-            <Newline/>
+        <Box borderStyle='single' padding={2}>
             {
-                data.map(({id, name, current_price, price_change_percentage_24h, ath}) => (
-                    <Box key={id}>
-                       <Box width='25%'>
-                            <Text>{name}</Text>
-                        </Box>
-                        <Box width='25%'>
-                            <Text color='cyan'>{'$' + current_price.toLocaleString()}</Text>
-                        </Box>
-                        <Box width='25%'>
-                            <Text backgroundColor={Math.sign(price_change_percentage_24h) < 0 ? 'red' : 'green'}>
-                                {price_change_percentage_24h.toFixed(2) + '%'}
-                            </Text>
-                        </Box>
-                        <Box width='25%'>
-                            <Text color='green'>{'$' + ath.toLocaleString()}</Text>
-                        </Box>
+                data.length === 0 ?
+                <Box>
+                    <Text>Loading ...</Text>
+                </Box> :
+                <Box flexDirection='column'>
+                    <Box>
+                        <Box width='25%'><Text>COIN</Text></Box>
+                        <Box width='25%'><Text>CURRENT PRICE (USD)</Text></Box>
+                        <Box width='25%'><Text>24 HOUR CHANGE</Text></Box>
+                        <Box width='25%'><Text>ALL TIME HIGH</Text></Box>
                     </Box>
-                ))
+                    <Newline/>
+                    {
+                        data.map(({id, name, current_price, price_change_percentage_24h, ath}) => (
+                            <Box key={id}>
+                                <Box width='25%'>
+                                    <Text>{name}</Text>
+                                </Box>
+                                <Box width='25%'>
+                                    <Text color='cyan'>{'$' + current_price.toLocaleString()}</Text>
+                                </Box>
+                                <Box width='25%'>
+                                    <Text backgroundColor={Math.sign(price_change_percentage_24h) < 0 ? 'red' : 'green'}>
+                                        {price_change_percentage_24h.toFixed(2) + '%'}
+                                    </Text>
+                                </Box>
+                                <Box width='25%'>
+                                    <Text color='green'>{'$' + ath.toLocaleString()}</Text>
+                                </Box>
+                            </Box>
+                        ))
+                    }
+                </Box>
             }
         </Box>
     )
