d563e6898be2dbff4c7764c90e278205a95f2931
fbrv01 <fbucholc@gmail.com>
2022-07-03 23:07:03 +0200

    added some decorations using ink

--- components/Table.js
+++ components/Table.js
@@ -29,10 +29,20 @@ const Table = () => {
             {
                 data.map(({id, name, current_price, price_change_percentage_24h, ath}) => (
                     <Box key={id}>
-                        <Box width='25%'><Text>{name}</Text></Box>
-                        <Box width='25%'><Text>{current_price}</Text></Box>
-                        <Box width='25%'><Text>{price_change_percentage_24h}</Text></Box>
-                        <Box width='25%'><Text>{ath}</Text></Box>
+                       <Box width='25%'>
+                            <Text>{name}</Text>
+                        </Box>
+                        <Box width='25%'>
+                            <Text color='cyan'>{'$' + current_price.toLocaleString()}</Text>
+                        </Box>
+                        <Box width='25%'>
+                            <Text backgroundColor={Math.sign(price_change_percentage_24h) < 0 ? 'red' : 'green'}>
+                                {price_change_percentage_24h.toFixed(2) + '%'}
+                            </Text>
+                        </Box>
+                        <Box width='25%'>
+                            <Text color='green'>{'$' + ath.toLocaleString()}</Text>
+                        </Box>
                     </Box>
                 ))
             }
